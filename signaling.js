45ceccccdad83b46d9615e356fbe5fc9c28b3de8
Erik Xavier <erik.xam@gmail.com>
2015-05-01 01:07:44 -0300

    Separação de aruiqvo signaling.js

--- signaling.js
+++ signaling.js
@@ -1,1 +1,24 @@
 /// <reference path="typings/node/node.d.ts"/>
+function signaling(server) {
+	var users = [];	
+	var io = require('socket.io')(server);
+	
+	io.on('connection', function(socket) {
+	  
+	  console.log('usuario conectou');
+	  socket.on('entrar', function(msg) {
+	    console.log(msg+' entrou');
+	    var usuario = {
+	      id: users.length+1,
+	      nick: msg
+	    };
+	    users.push(usuario);
+	    socket.emit('conected' , usuario.nick);    
+	    io.emit('lista', JSON.stringify(users) );
+	  })
+	  
+	return io;  
+	})	
+}
+
+module.exports = signaling;

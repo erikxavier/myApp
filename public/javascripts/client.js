9894f03fd7650da6d06c896eeb4511838902923d
Erik Xavier <erik.xam@gmail.com>
2015-04-30 22:21:13 -0300

    Aula 30-04

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,5 +1,22 @@
 /// <reference path="../../typings/jquery/jquery.d.ts"/>
 $(function() {	
+	
+	var socket = io();
+	$('#conectar').click(function() {
+		socket.emit('entrar', $('#nick').val() );
+	});
+	socket.on('conected', function(msg) {
+		$('#status').text('Conectado com ID '+msg);
+	})
+	
+	socket.on('lista', function(json) {
+		var lista = JSON.parse(json);		
+		$('#users').empty();
+		lista.forEach(function(user) {
+			console.log(user.nick);
+			$('#users').append('<option value="'+user.nick+'">'+user.nick+'</option>');
+		})
+	})		
 	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 	var constraints = {audio:true, video:false};
 	var localAudio = $('#localAudio');
@@ -15,5 +32,4 @@ $(function() {
 	$('#call').click(function() {
 		navigator.getUserMedia(constraints, getMediaOk, getMediaKo);	
 	});
-	
 })
